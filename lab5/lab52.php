fa4cd0e9117b9de67ea62c0c33a8dbf6c8268a54
DQIU1712 <95393137+DQIU1712@users.noreply.github.com>
2022-09-28 12:02:28 -0500

    Add files via upload

--- lab5/lab52.php
+++ lab5/lab52.php
@@ -4,22 +4,19 @@ if(is_uploaded_file($_FILES['imagen']['tmp_name']))
     $nombreDirectorio="archivos/";
     $sizeArchivo=$_FILES['imagen']['size'];
     $nombrearchivo=$_FILES['imagen']['name'];
-    $extension=$_FILES['imagen']['type'];
     $nombreCompleto=$nombreDirectorio.$nombrearchivo;
 
     
-       
-        if (is_file($extension)=="jpg" || is_file($extension) =="gif" || is_file($extension)=="png" || is_file($extension)=="jpeg"){
-            if(is_file($sizeArchivo)<1048576){
-            move_uploaded_file ($_FILES['imagen']['tmp_name'],
-            $nombreDirectorio.$nombrearchivo);
+       if(is_file($sizeArchivo)<=1048576 &&($_FILES['imagen']['type']=="image/jpeg"||$_FILES['imagen']['type']=="image/jpg"||$_FILES['imagen']['type']=="image/png"||$_FILES['imagen']['type']=="image/gif")){
+            
+                move_uploaded_file ($_FILES['imagen']['tmp_name'],
+                $nombreDirectorio.$nombrearchivo);
 
-            echo "El archivo se ha subido sastifactoriamente al directorio
-            $nombreDirectorio <br>";
-        }else echo "El imagen subido no puede ser mayor de 1 mb, debe reducirlo";
-        
-        } else echo "No es un formato válido.Solo puede subir imagen(jpg, jpeg, gif y png)";
-
+                echo "El archivo se ha subido sastifactoriamente al directorio
+                $nombreDirectorio <br>";
+                
+        } else 
+        echo "No es un formato válido.Solo puede subir imagen(jpg, jpeg, gif y png) y el tamaño del imagen no mayor a 1mb";
 }
 else
     echo "No se ha podido subir el archivo <br>";
